f10168fe219300e7440e34751919654240b292fc
edwin thierry <edwin.thierry350@gmail.com>
2024-07-02 19:59:48 +0200

    Home page & game page init

--- trailer_moon/core/views.py
+++ trailer_moon/core/views.py
@@ -3,8 +3,11 @@ from django.views.generic import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-from characters.models import Character
-from trailer.models import Trailer
+from characters.models import *
+from trailers.models import *
+from locations.models import *
+
+
 
 class HomePageView(View):
     template_name = "core/home.html"
@@ -12,14 +15,30 @@ class HomePageView(View):
     def get(self, request):
         user_connected = request.user
         return render(request, self.template_name, {'user_connected': user_connected})
+    
 
 @method_decorator(login_required, name='dispatch')
 class GameBoardView(View):
     template_name = "core/game_board.html"
 
     def get(self, request):
-        trailers = Trailer.objects.filter(user=request.user).prefetch_related('characters')
+        trailers = Trailer.objects.filter(user=request.user)
         context = {
-            'trailers': trailers
+            'trailers': trailers,
+        }
+        return render(request, self.template_name, context=context)
+    
+
+class GameMainView(View):
+    template_name = "core/game_main.html"
+
+    def get(self, request):
+        trailers = Trailer.objects.filter(user=request.user)
+        map = request.user.current_map
+        locations = Location.objects.filter(map=map)
+        context = {
+            'trailers' : trailers,
+            'locations': locations,
+            'map': map,
         }
         return render(request, self.template_name, context=context)
